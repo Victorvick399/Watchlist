b1c54e542cffbf58de0e62ca1bd22474b5a114c8
Victorvick399 <victorvick399@gmail.com>
2019-11-22 12:27:29 +0300

    fix email error

--- start.sh
+++
@@ -1,5 +0,0 @@
-export MOVIE_API_KEY='77f93e10c6a0f45e124157eeca9d1a7c'
-export SECRET_KEY='Victor'
-export MAIL_USERNAME='vikingwatchlist25@gmail.com'
-export MAIL_PASSWORD='vikwatchlist'
-python3.6 manage.py server
